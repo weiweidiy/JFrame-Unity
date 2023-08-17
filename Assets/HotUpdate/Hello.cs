81bb8355d5ba3786566f99cf4f3084cd87c9d2d8
jichunwei <49595272@qq.com>
2023-08-17 23:26:00 +0800

    add launcher

--- Assets/HotUpdate/Hello.cs
+++ Assets/HotUpdate/Hello.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using DG.Tweening;
 using UnityEngine;
 
 public class Hello
---
+++ Assets/Scripts/Launcher.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Cysharp.Threading.Tasks;
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+//using DG.Tween;
+
+public class Launcher : MonoBehaviour
+{
+    // Start is called before the first frame update
+    async void Start()
+    {
+        //播放splash
+        var splashTask = PlaySplash();
+
+        //初始化YooAssets
+        var initTask= InitYooAssets();
+
+        //等待2个任务完成
+        await UniTask.WhenAll(splashTask, initTask);
+
+        //切换场景
+        SceneManager.LoadScene("Main");
+    }
+
+    /// <summary>
+    /// 播放splash动画
+    /// </summary>
+    /// <returns></returns>
+    /// <exception cref="NotImplementedException"></exception>
+    private UniTask PlaySplash()
+    {
+        return UniTask.Delay(5000);
+    }
+
+    /// <summary>
+    /// 初始化YooAssets
+    /// </summary>
+    /// <returns></returns>
+    /// <exception cref="NotImplementedException"></exception>
+    private UniTask InitYooAssets()
+    {
+        return UniTask.Delay(1000);
+    }
+}
